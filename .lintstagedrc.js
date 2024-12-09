49b6122dbc4fb8b3b72ea88614e4306106c18c86
Loris <lorennarezennde@gmail.com>
2024-12-09 19:59:40 -0300

    teste

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
-export default {
+module.exports =  {
   '*.{js, jsx, ts, tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run lint --fix . ${filenames.join(' --file')}`
