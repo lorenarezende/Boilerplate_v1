916fa69a321f3eb86aeb04341164a07ee9717db3
Loris <lorennarezennde@gmail.com>
2024-12-08 23:23:14 -0300

    first commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,6 @@
+// export default {
+//   '*.{js, jsx, ts, tsx}': (filenames) => [
+//     `prettier --write ${filenames.join(' ')}`,
+//     `npm run lint --fix . ${filenames.join(' --file')}`
+//   ]
+// }
