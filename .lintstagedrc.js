24d010d2959c638b3bbd84251087b3499cdcbb94
Loris <lorennarezennde@gmail.com>
2024-12-09 19:59:03 -0300

    teste

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
-// export default {
-//   '*.{js, jsx, ts, tsx}': (filenames) => [
-//     `prettier --write ${filenames.join(' ')}`,
-//     `npm run lint --fix . ${filenames.join(' --file')}`
-//   ]
-// }
+export default {
+  '*.{js, jsx, ts, tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`
+  ]
+}
