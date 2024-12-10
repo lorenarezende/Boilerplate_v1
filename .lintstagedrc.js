835f59a08be5cace335ce23397246dc1b2994caf
Loris <lorennarezennde@gmail.com>
2024-12-09 21:23:29 -0300

    agora vai

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
-module.exports =  {
+module.exports = {
   '*.{js, jsx, ts, tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run lint --fix . ${filenames.join(' --file')}`
