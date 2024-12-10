9260255ff6dfa07d7c9f87918b55055aebe0272f
Loris <lorennarezennde@gmail.com>
2024-12-09 21:56:50 -0300

    Jest config

---
+++ app/jest.config.js
@@ -0,0 +1,9 @@
+export const TestEnvironment = 'jsdom'
+export const testPathIgnorePatterns = ['/node_modules/', '/.next/']
+export const collectCoverage = true
+export const collectCoverageFrom = ['src/**/*.ts(x)?']
+export const setupFilesAfterEnv = ['<rootDir>/.jest/setup.ts']
+export const modulePaths = ['<rootDir>/src/']
+export const transform = {
+  '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+}
